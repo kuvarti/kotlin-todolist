ac2e94603294cd0a3cd6795b98f91a077dfb3226
Kuvarti <uzali.kafasi@gmail.com>
2022-12-02 07:17:49 +0300

    Set new gradle settings and prepare the layout

--- app/src/main/java/tr/tutorials/kotlin_todolist/MainActivity.kt
+++ app/src/main/java/tr/tutorials/kotlin_todolist/MainActivity.kt
@@ -2,10 +2,18 @@ package tr.tutorials.kotlin_todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import tr.tutorials.kotlin_todolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+	private lateinit var binding: ActivityMainBinding
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+		binding = ActivityMainBinding.inflate(layoutInflater)
+		setContentView(binding.root)
+
+
+
 	}
 }
