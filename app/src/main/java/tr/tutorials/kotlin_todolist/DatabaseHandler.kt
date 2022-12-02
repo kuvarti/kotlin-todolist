666185af70799d584dee8ee70fd6f953faa0a9c2
Kuvarti <uzali.kafasi@gmail.com>
2022-12-02 20:07:27 +0300

    Creating tables

--- app/src/main/java/tr/tutorials/kotlin_todolist/DatabaseHandler.kt
+++ app/src/main/java/tr/tutorials/kotlin_todolist/DatabaseHandler.kt
@@ -1,6 +1,8 @@
 package tr.tutorials.kotlin_todolist
 
+import android.content.ContentValues
 import android.content.Context
+import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 class DatabaseHandler(Context: Context) :
@@ -11,5 +13,12 @@ class DatabaseHandler(Context: Context) :
 		private const val DATABASE_VERSION = 1
 	}
 
-
+	override fun onCreate(db: SQLiteDatabase?) {
+		val UsersTableCreate = ("CREATE TABLE users ( " +
+		 	"id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, passw text )")
+		db?.execSQL(UsersTableCreate)
+		val ContentTableCreate = ("CREATE TABLE contents ( " +
+		 	"fromuser INTEGER, content TEXT NOT NULL, FOREIGN KEY(fromuser) REFERENCES users (id))")
+		db?.execSQL(ContentTableCreate)
+	}
 }
