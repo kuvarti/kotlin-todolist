dcd9f4e0e06245cce5cfc1d7d6de9192dfcb57bf
Kuvarti <uzali.kafasi@gmail.com>
2022-12-03 12:19:22 +0300

    add convert list method

--- app/src/main/java/tr/tutorials/kotlin_todolist/contentModelClass.kt
+++ app/src/main/java/tr/tutorials/kotlin_todolist/contentModelClass.kt
@@ -1,3 +1,12 @@
 package tr.tutorials.kotlin_todolist
 
-class contentModelClass (val fromuser: Int, val content: String)
+class contentModelClass (val fromuser: Int, val content: String){
+	fun convertlist(lst: ArrayList<contentModelClass>): MutableList<String>{
+		val ret: MutableList<String> = mutableListOf()
+
+		for (x in lst){
+			ret.add(x.content)
+		}
+		return ret
+	}
+}
